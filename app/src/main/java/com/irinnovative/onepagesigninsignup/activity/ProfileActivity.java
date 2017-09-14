13b84930f9ad83aa6dd8cd79f7ea75099f8988ed
thabang <thabangjohannes2@gmail.com>
2017-09-14 15:54:36 +0200

    Merge remote-tracking branch 'remotes/origin/list-abuse'
    # Conflicts:
    #	app/src/main/AndroidManifest.xml

--- app/src/main/java/com/irinnovative/onepagesigninsignup/activity/ProfileActivity.java
+++ app/src/main/java/com/irinnovative/onepagesigninsignup/activity/ProfileActivity.java
@@ -1,25 +1,66 @@
 package com.irinnovative.onepagesigninsignup.activity;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.irinnovative.onepagesigninsignup.R;
 
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
 public class ProfileActivity extends AppCompatActivity {
+    private ImageView imProfilePic;
+    int RESULT_LOAD_IMG = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
+        imProfilePic = (ImageView) findViewById(R.id.ImageView_user_pic);
+        imProfilePic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+                photoPickerIntent.setType("image/*");
+                startActivityForResult(photoPickerIntent, RESULT_LOAD_IMG);
+            }
+        });
 
 
 
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            try {
+                final Uri imageUri = data.getData();
+                final InputStream imageStream = getContentResolver().openInputStream(imageUri);
+                final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+                imProfilePic.setImageBitmap(selectedImage);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                Toast.makeText(ProfileActivity.this, "Something went wrong", Toast.LENGTH_LONG).show();
+            }
+
+        }else {
+            Toast.makeText(ProfileActivity.this, "You haven't picked Image",Toast.LENGTH_LONG).show();
+        }
     }
 
     @Override
